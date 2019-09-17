c77f6ed83ec6ce033f6bd4f20d11c19b061867a4
industrious-modal-4420 <khaledhassan45@gmail.com>
2019-09-17 20:23:43 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,9 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if BRANDS.include?(brand) == false
-      BRANDS << brand
-    end
+    BRAND << brand if !BRANDS.include?(brand)
   end
 
   def cobble
